52c2c75268657ef76d4f615cec53b2c439ac5361
si_dean_ako <dean.casili@gmail.com>
2013-11-24 07:48:24 +0800

    updates

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -76,7 +76,7 @@ angular.module('myApp.controllers', ['LocalStorageModule']).
           console.log('if');
 	        Api.createGlobeAccessToken(code, creds.user.id).then(function(result) {
             console.log('createGlobeAccessToken');
-            rootScope.$broadcast("event: successGetToken");
+            rootScope.$broadcast("event:loggedIn");
     				console.log(result);
     			}, function(result) {
             //redirect to login facebook
@@ -120,10 +120,6 @@ angular.module('myApp.controllers', ['LocalStorageModule']).
       console.log(id);
       session.add("id", id);
     };
-
-    scope.$on("event: successGetToken", function() {
-      scope.hasGlobeAccessToken = (Utils.isEmpty(creds)) ? "" : creds.user.has_globe_access_token;
-    });
 
     scope.$on("event:loggedOut", function() {
       console.log('boom');
